370376f3ed1f4fe6475fa0cda31683421329bf10
Tan De Wei <dewei@Tans-MacBook-Air.local>
2022-08-24 15:27:04 +0800

    include esli fix and .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  fetch("https://wger.de/api/v2/exerciseinfo/?limit=20&offset=20")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  return (
+    <div className="App">
+      <h1> Testing</h1>
+      <h2>{data.name}</h2>
+    </div>
+  );
 }
 
 export default App;
