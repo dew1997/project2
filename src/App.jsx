276ff24c742b3c862a0072ea3182c1e89dc8ca83
Tan De Wei <dewei@Tans-MacBook-Air.local>
2022-08-25 22:23:39 +0800

    setup route for About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import About from "./Pages/About";
+import Home from "./Pages/About";
 // import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 function App() {
   const [exercise, setExercise] = useState([]);
   fetch("https://wger.de/api/v2/exercise/?format=json")
     .then((response) => response.json())
-    .then((data) => setExercise(data.results[0].name));
+    .then((data) => setExercise(data.results[10].name));
 
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/About" element={<About />} />
+        </Routes>
+        <h1> Testing</h1>
+        <h2>{exercise}</h2>
       </BrowserRouter>
-      <h1> Testing</h1>
-      <h2>{exercise}</h2>
     </div>
   );
 }
---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        BarbellFlies is a website where you can find exercises that can be
+        implemented in your next workout! Suitable for beginners who are looking
+        to create a customisable workout that targets the muscle group you want
+        to.
+      </p>
+    </div>
+  );
+}
+
+export default About;
